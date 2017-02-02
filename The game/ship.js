c17604dd3e83b72bd646900ec2b201865ec972de
Wilyvonka <viljargjerde@hotmail.com>
2017-02-02 13:44:07 +0100

    Update ship.js

--- The game/ship.js
+++ The game/ship.js
@@ -21,7 +21,7 @@ function Ship() {
   };
   this.boost = function() {
     var force = p5.Vector.fromAngle(this.heading);
-    force.mult(0.3);
+    force.mult(0.35);
     this.vel.add(force);
   };
 
